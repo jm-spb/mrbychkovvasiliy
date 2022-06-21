41f2bd9f463b1fe70ee87c7fa99f41afc1308a14
ivan <tuchel.ivan@gmail.com>
2022-06-21 23:42:19 +0300

    init Layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Footer from '../Footer';
 import Navbar from '../Navbar';
+import '../../styles/normalize.scss';
+import * as styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
+  console.log('INDEX');
   return (
-    <>
+    <div className={styles.container}>
       <Navbar />
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
 
