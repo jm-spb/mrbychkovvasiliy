244f820483fecb692825619783cddd511ddaef00
ivan <tuchel.ivan@gmail.com>
2022-06-22 01:12:57 +0300

    add StaticImage, custom hook, fix layout

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,75 +1,68 @@
-import { Link } from 'gatsby';
 import React from 'react';
+import * as styles from './Navbar.module.scss';
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import { HamburgerSqueeze } from 'react-animated-burgers';
 import classNames from 'classnames';
-import logo from '../../assets/logo-medium.webp';
 import { additionalNavLinks, mainNavLinks } from '../../content';
-import * as styles from './Navbar.module.scss';
-import { useOnClickOutside } from '../../hooks/useOnClick';
+import { useOnClickOutside } from '../../hooks/useOnClickOutside';
 
 const Navbar = () => {
   const [isActive, setIsActive] = React.useState(false);
   const ref = React.useRef(null);
-  useOnClickOutside(ref, () => setIsActive(false));
-  // const act = React.useRef(false);
-  console.log(isActive);
-
   const toggleButton = () => setIsActive((prevState) => !prevState);
-  // const toggleButton = () => {
-  //   console.log('toggle');
-  //   if (!isActive) {
-  //     document.addEventListener('click', handleOutsideClick);
-  //   } else {
-  //     document.removeEventListener('click', handleOutsideClick);
-  //   }
-
-  //   setIsActive((prevState) => !prevState);
-  // };
-
-  // const handleOutsideClick = (e) => {
-  //   console.log('handle');
-  //   console.log(isActive);
-
-  //   if (!ref.current.contains(e.target)) {
-  //     toggleButton();
-  //   }
-  // };
-
   const closeMenu = () => setIsActive(false);
+  useOnClickOutside(ref, () => setIsActive(false));
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.header}>
         <Link to="/" className={styles.logoName}>
-          <img src={logo} alt="Центр развития охраны труда" width={80} height={80} />
+          <StaticImage
+            src="../../assets/logo-medium.webp"
+            alt="Центр развития охраны труда"
+            width={80}
+            height={80}
+            placeholder="blurred"
+            layout="fixed"
+          />
           <span className={styles.name}>Центр развития охраны труда</span>
         </Link>
         <button className={styles.button}>Консультация</button>
       </div>
-      <div ref={ref} className={styles.links}>
-        <div className={classNames(styles.container, { [styles.isActive]: isActive })}>
-          {mainNavLinks.map(({ name, link }) => (
-            <Link key={name} to={link} className={styles.mainLink} onClick={closeMenu}>
-              {name}
-            </Link>
-          ))}
-          <div className={styles.dropdown}>
-            <span className={styles.more}>Ещё</span>
-            <div className={styles.dropdownContent}>
-              {additionalNavLinks.map(({ name, link }) => (
-                <Link key={name} to={link} className={styles.addLink} onClick={closeMenu}>
-                  {name}
-                </Link>
-              ))}
+      <div className={styles.links}>
+        <div className={styles.container}>
+          <div className={classNames(styles.menu, { [styles.isActive]: isActive })}>
+            {mainNavLinks.map(({ name, link }) => (
+              <Link key={name} to={link} className={styles.mainLink} onClick={closeMenu}>
+                {name}
+              </Link>
+            ))}
+            <div className={styles.dropdown}>
+              <span className={styles.more}>Ещё</span>
+              <div className={styles.dropdownContent}>
+                {additionalNavLinks.map(({ name, link }) => (
+                  <Link
+                    key={name}
+                    to={link}
+                    className={styles.addLink}
+                    onClick={closeMenu}
+                  >
+                    {name}
+                  </Link>
+                ))}
+              </div>
             </div>
           </div>
+          <span className={styles.phoneNumber}>8-800-505-20-41</span>
+          <div ref={ref} className={styles.hamburger}>
+            <HamburgerSqueeze
+              buttonWidth={24}
+              barColor="white"
+              {...{ isActive, toggleButton }}
+            />
+          </div>
         </div>
-        <HamburgerSqueeze
-          className={styles.hamburger}
-          buttonWidth={24}
-          barColor="white"
-          {...{ isActive, toggleButton }}
-        />
       </div>
     </nav>
   );
