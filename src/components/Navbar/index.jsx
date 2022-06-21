70b740e6dd3fdc0cd832fc4e69e64920db658772
ivan <tuchel.ivan@gmail.com>
2022-06-21 23:41:16 +0300

    hide menu on outside click

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,10 +5,37 @@ import classNames from 'classnames';
 import logo from '../../assets/logo-medium.webp';
 import { additionalNavLinks, mainNavLinks } from '../../content';
 import * as styles from './Navbar.module.scss';
+import { useOnClickOutside } from '../../hooks/useOnClick';
 
 const Navbar = () => {
   const [isActive, setIsActive] = React.useState(false);
+  const ref = React.useRef(null);
+  useOnClickOutside(ref, () => setIsActive(false));
+  // const act = React.useRef(false);
+  console.log(isActive);
+
   const toggleButton = () => setIsActive((prevState) => !prevState);
+  // const toggleButton = () => {
+  //   console.log('toggle');
+  //   if (!isActive) {
+  //     document.addEventListener('click', handleOutsideClick);
+  //   } else {
+  //     document.removeEventListener('click', handleOutsideClick);
+  //   }
+
+  //   setIsActive((prevState) => !prevState);
+  // };
+
+  // const handleOutsideClick = (e) => {
+  //   console.log('handle');
+  //   console.log(isActive);
+
+  //   if (!ref.current.contains(e.target)) {
+  //     toggleButton();
+  //   }
+  // };
+
+  const closeMenu = () => setIsActive(false);
 
   return (
     <nav className={styles.navbar}>
@@ -19,14 +46,10 @@ const Navbar = () => {
         </Link>
         <button className={styles.button}>Консультация</button>
       </div>
-      <div className={styles.links}>
+      <div ref={ref} className={styles.links}>
         <div className={classNames(styles.container, { [styles.isActive]: isActive })}>
           {mainNavLinks.map(({ name, link }) => (
-            <Link
-              to={link}
-              className={styles.mainLink}
-              // onClick={() => setIsActive(false)}
-            >
+            <Link key={name} to={link} className={styles.mainLink} onClick={closeMenu}>
               {name}
             </Link>
           ))}
@@ -34,11 +57,7 @@ const Navbar = () => {
             <span className={styles.more}>Ещё</span>
             <div className={styles.dropdownContent}>
               {additionalNavLinks.map(({ name, link }) => (
-                <Link
-                  to={link}
-                  className={styles.addLink}
-                  // onClick={() => setIsActive(false)}
-                >
+                <Link key={name} to={link} className={styles.addLink} onClick={closeMenu}>
                   {name}
                 </Link>
               ))}
