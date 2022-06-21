3d35853e89e3b1d1af40d577a4e01e1522e90d21
ivan <tuchel.ivan@gmail.com>
2022-06-21 23:41:45 +0300

    init Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'gatsby';
+import * as styles from './Footer.module.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className={styles.top}>
+        <Link to="/" className={styles.heading}>
+          Центр развития охраны труда
+        </Link>
+        <a href="mailto:Ohranatryda@inbox.ru" className={styles.email}>
+          Ohranatryda@inbox.ru
+        </a>
+        <span>8-800-505-20-41</span>
+      </div>
+      <div className={styles.bottom}>
+        <span>
+          &copy;{`${new Date().getFullYear()} ООО "Центр развития охраны труда"`}
+        </span>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
