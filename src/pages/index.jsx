d1ae5291338c321d57e8422eadd7b32a7637522d
ivan <tuchel.ivan@gmail.com>
2022-06-24 03:37:34 +0300

    create first slide & mission slide

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import * as styles from './index.module.scss';
+import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/Layout';
 
 const Home = () => {
   return (
     <Layout>
-      <div>Home</div>
+      <main>
+        <article className={styles.occupational}>
+          <StaticImage
+            src="../assets/images/index/index-1.webp"
+            alt="Центр развития охраны труда"
+            placeholder="blurred"
+            layout="fullWidth"
+          />
+          <div className={styles.container}>
+            <section className={styles.section}>
+              <h1 className={styles.heading}>ОХРАНА ТРУДА</h1>
+              <h2 className={styles.subHeading}>для КОМПАНИЙ, СПЕЦИАЛИСТОВ, СТУДЕНТОВ</h2>
+              <div className={styles.text}>
+                <p className={styles.paragraph}>
+                  Помогаем компаниям решать нестандартные вопросы по охране труда
+                </p>
+                <p className={styles.paragraph}>
+                  Участвуем в проверках, расследуем НС по всей территории РФ
+                </p>
+                <p className={styles.paragraph}>
+                  Реализуем нестандартные задачи в области охраны труда и безопасности
+                </p>
+                <p className={styles.paragraph}>
+                  Развиваем студентов и специалистов в области охраны труда
+                </p>
+                <p className={styles.paragraph}>
+                  Помогаем компаниям создавать эффективные системы безопасности
+                  производства
+                </p>
+              </div>
+              <div className={styles.buttons}>
+                <button className={styles.servicesBtn}>Услуги для бизнеса</button>
+                <button className={styles.questionBtn}>Срочный вопрос</button>
+              </div>
+            </section>
+          </div>
+        </article>
+        <article className={styles.mission}>
+          <section className={styles.missionSection}>
+            <div className={styles.missionContent}>
+              <h2 className={styles.missionHeading}>НАША МИССИЯ:</h2>
+              <p className={styles.missionParagraph}>
+                "Проводить мероприятия, которые будут развивать охрану труда. Создавать
+                условия для развития студентов и специалистов в области Техносферной
+                безопасности. Предоставлять качественные услуги по охране труда"
+              </p>
+              <button className={styles.missionBtn}>Присоединиться к нам</button>
+            </div>
+            <StaticImage
+              src="../assets/images/index/logo-large.webp"
+              alt="Центр развития охраны труда"
+              className={styles.missionLogo}
+              placeholder="blurred"
+              layout="constrained"
+            />
+          </section>
+        </article>
+      </main>
     </Layout>
   );
 };
