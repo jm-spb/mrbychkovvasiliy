77f9e2258435568162c1c300d3dfb228be30f400
ivan <tuchel.ivan@gmail.com>
2022-07-07 21:21:22 +0300

    add content in page

--- src/pages/work-examples/index.jsx
+++ src/pages/work-examples/index.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
+import * as styles from './work-examples.module.scss';
+import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../../components/Layout';
 
-const WorkExamples = () => {
+const WorkExamples = ({ data }) => {
+  const { workExamples } = data.allContentJson.edges[1].node;
+
   return (
     <Layout>
-      <div>WorkExamples</div>
+      <main>
+        <section className={styles.container}>
+          <h1 className={styles.mainHeading}>Примеры работ</h1>
+          <p className={styles.paragraph}>
+            Для того что бы вам, было легче узнать похожую ситуацию и увидеть пути решения
+          </p>
+          <section className={styles.examples}>
+            {workExamples.map(({ imgSrc, heading, text }) => {
+              const image = getImage(imgSrc);
+
+              return (
+                <article key={heading} className={styles.example}>
+                  <GatsbyImage image={image} alt={heading} />
+                  <div>
+                    <h2 className={styles.exampleHeading}>{heading}</h2>
+                    <p className={styles.paragraph}>{text}</p>
+                  </div>
+                </article>
+              );
+            })}
+          </section>
+        </section>
+      </main>
     </Layout>
   );
 };
 
 export default WorkExamples;
+
+export const query = graphql`
+  {
+    allContentJson {
+      edges {
+        node {
+          workExamples {
+            heading
+            text
+            imgSrc {
+              childImageSharp {
+                gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
