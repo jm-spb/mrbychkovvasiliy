8af2d7fe8dd153c811b43ab349c8d4f6efc5ba6a
ivan <tuchel.ivan@gmail.com>
2022-07-11 21:33:18 +0300

    refactor useOnClickOutside hook

--- src/hooks/useOnClickOutside.js
+++ src/hooks/useOnClickOutside.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
 export function useOnClickOutside(ref, handler) {
-  React.useEffect(
-    () => {
-      const listener = (e) => {
-        if (!ref.current || ref.current.contains(e.target)) {
-          return;
-        }
-        handler(e);
-      };
-      document.addEventListener("click", listener);
-      return () => {
-        document.removeEventListener("click", listener);
-      };
-    },   
-    [ref, handler]
-  );
+  React.useEffect(() => {
+    const listener = (e) => {   
+      if (ref.current === e.target) {
+        handler();
+      }
+    };
+    document.addEventListener('click', listener);
+
+    return () => {
+      document.removeEventListener('click', listener);
+    };
+  }, [ref, handler]);
 }
