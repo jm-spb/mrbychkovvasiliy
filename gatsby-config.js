f3083226374e3c96887a40627dfee2ee03cae9e3
ivan <tuchel.ivan@gmail.com>
2022-08-11 21:19:09 +0300

    add siteMetadat, gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
 module.exports = {
   siteMetadata: {
     title: `mrbychkovvasiliy`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-transformer-json',
+    'gatsby-plugin-smoothscroll',
+    'gatsby-plugin-scroll-reveal',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src`,
+      },
+    },
+    'gatsby-plugin-react-helmet'
+  ],
 };
