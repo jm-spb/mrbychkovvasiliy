fa230775df9baee3d7327c73b2e4b19173d75d9e
ivan <tuchel.ivan@gmail.com>
2022-08-12 23:30:06 +0300

    add form validation
    Name: min & max length, only latin & cyrillic letters
    Phone: add phone mask

--- src/components/AppForm/index.jsx
+++ src/components/AppForm/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as styles from './AppForm.module.scss';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
+import InputMask from 'react-input-mask';
 import axios from 'axios';
 
 const AppForm = () => {
@@ -9,6 +10,7 @@ const AppForm = () => {
     formState: { errors, isValid, isSubmitSuccessful },
     handleSubmit,
     reset,
+    control,
   } = useForm({
     mode: 'onBlur',
   });
@@ -19,13 +21,11 @@ const AppForm = () => {
     }
   }, [isSubmitSuccessful, reset]);
 
-  const onSubmit = ({ Name, rawPhoneNumber }) => {
-    axios
-      .post('https://sheet.best/api/sheets/52b79214-9dc4-4c7f-bac7-8ef4c1d92c73', {
-        Name,
-        rawPhoneNumber,
-      })
-      .then((res) => console.log(res));
+  const onSubmit = ({ Name, Phone }) => {
+    axios.post('https://sheet.best/api/sheets/52b79214-9dc4-4c7f-bac7-8ef4c1d92c73', {
+      Name,
+      Phone,
+    });
   };
 
   return (
@@ -35,6 +35,18 @@ const AppForm = () => {
         <input
           {...register('Name', {
             required: 'Укажите своё имя',
+            minLength: {
+              value: 3,
+              message: 'Введите от 3 до 24 символов',
+            },
+            maxLength: {
+              value: 24,
+              message: 'Введите от 3 до 24 символов',
+            },
+            pattern: {
+              value: /^[A-Za-zЁёА-я]+$/g,
+              message: 'Имя должно содержать только латиницу или кириллицу',
+            },
           })}
           className={styles.input}
           id="Name"
@@ -45,22 +57,35 @@ const AppForm = () => {
       <div className={styles.errorBox}>
         {errors?.Name && <span className={styles.error}>{errors?.Name.message}</span>}
       </div>
-      <label className={styles.label} htmlFor="rawPhoneNumber">
+      <label className={styles.label} htmlFor="Phone">
         Телефон
-        <input
-          {...register('rawPhoneNumber', {
-            required: 'Укажите номер телефона',
-          })}
-          className={styles.input}
-          id="rawPhoneNumber"
-          type="tel"
-          placeholder="Ваш номер телефона"
-        />
       </label>
-      <div className={styles.errorBox}>
-        {errors?.rawPhoneNumber && (
-          <span className={styles.error}>{errors?.rawPhoneNumber.message}</span>
+      <Controller
+        name="Phone"
+        control={control}
+        defaultValue=""
+        rules={{
+          required: true,
+        }}
+        render={() => (
+          <InputMask
+            className={styles.input}
+            mask="8 (999) 999-99-99"
+            {...register('Phone', { required: 'Укажите номер телефона' })}
+          >
+            {(inputProps) => (
+              <input
+                {...inputProps}
+                id="Phone"
+                type="tel"
+                placeholder="Ваш номер телефона"
+              />
+            )}
+          </InputMask>
         )}
+      />
+      <div className={styles.errorBox}>
+        {errors?.Phone && <span className={styles.error}>{errors?.Phone.message}</span>}
       </div>
       <div className={styles.btn}>
         <button className={styles.submit} type="submit" disabled={!isValid}>
