ba80146a4a608ec5dd061ca497a4dba20cfca610
ivan <tuchel.ivan@gmail.com>
2022-09-15 01:33:14 +0300

    create biotDocs content: download links

---
+++ src/content/biotDocs.js
@@ -0,0 +1,14 @@
+export const biotDocs = [
+  {
+    name: 'Положение о допуске подрядных организаций',
+    link: 'https://ohrana-truda.ru/activities/biot-2021/polozhenie.docx',
+  },
+  {
+    name: 'Приказ об организации работы с подрядными организациями',
+    link: 'https://ohrana-truda.ru/activities/biot-2021/prikaz.docx',
+  },
+  {
+    name: 'План Мероприятий (Заказчик-Подрядчик)',
+    link: 'https://ohrana-truda.ru/activities/biot-2021/plan.docx',
+  },
+];
