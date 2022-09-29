d1ff4f065c43d1bdd2daa5c4d4c670827f4aa205
ivan <tuchel.ivan@gmail.com>
2022-09-29 18:38:57 +0300

    refactor conditional rendering

--- src/components/Discount/index.jsx
+++ src/components/Discount/index.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import * as styles from './Discount.module.scss';
 import AppForm from '../AppForm';
 
-const Discount = ({ heading, text_1, text_2 }) => {
-  const videoMakingText = text_2 ? <p className={styles.paragraph}>{text_2}</p> : '';
-
+const Discount = ({ heading, mainText, additionalText }) => {
   return (
     <section className={styles.discount}>
       <article className={styles.content}>
         <h1 className={styles.heading}>{heading}</h1>
-        <p className={styles.paragraph}>{text_1}</p>
-        {videoMakingText || (
+        <p className={styles.paragraph}>{mainText}</p>
+        {additionalText ? (
+          <p className={styles.paragraph}>{additionalText}</p>
+        ) : (
           <p className={styles.paragraph}>
             Мы понимаем, что <strong>вопросы</strong> охраны труда и производственной
             безопасности носят неотлагательный характер и зачастую их решение{' '}
@@ -20,7 +20,6 @@ const Discount = ({ heading, text_1, text_2 }) => {
             <strong>в срок и конфиденциальность</strong> если ее требует заказчик.
           </p>
         )}
-
         <AppForm />
       </article>
     </section>
